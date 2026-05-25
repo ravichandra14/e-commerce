feb2210ae54b60c5a21454a5306a50e48a3a4a8f
ravichandra14 <ravichandra.b24@iiits.in>
2026-05-25 17:39:14 +0530

    chore: add nitro vercel preset configuration to resolve 404 deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,23 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { nitro } from "nitro/vite";
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      viteTsConfigPaths({
-        projects: ["./tsconfig.json"],
-      }),
-      tailwindcss(),
-      tanstackStart({
-        server: {
-          entry: "server",
-        },
-      }),
-      viteReact(),
-      mode === "production" ? cloudflare() : null,
-    ].filter(Boolean),
-    resolve: {
-      alias: {
-        "@": "/src",
-      },
+export default defineConfig({
+  plugins: [
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    nitro({
+      preset: "vercel",
+    }),
+    viteReact(),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
     },
-  };
+  },
 });
